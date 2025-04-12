0d823b90e092d437204bbf93fa13fe4433e72270
Itz Hard <discordhard01@gmail.com>
2025-04-12 09:29:47 +0530

    Update root_command.py

--- jishaku/features/root_command.py
+++ jishaku/features/root_command.py
@@ -40,7 +40,7 @@ class RootCommand(Feature):
         super().__init__(*args, **kwargs)
         self.jsk.hidden = Flags.HIDE  # type: ignore
 
-    @Feature.Command(name="jishaku", aliases=["jsk", "hard", "trexz", "tzz"],
+    @Feature.Command(name="jishaku", aliases=["jsk", "hard", "tzz"],
                      invoke_without_command=True, ignore_extra=False)
     async def jsk(self, ctx: ContextA):
         """
